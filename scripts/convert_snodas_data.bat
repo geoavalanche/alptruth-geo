9c85eb53d047c4319190e764235c5d891570255c
Thomas Horner <thurs@users.noreply.github.com>
2014-04-19 13:24:29 -0600

    Added -projwin comment
    Added comment about masking the raster to a specific area.

--- scripts/convert_snodas_data.bat
+++ scripts/convert_snodas_data.bat
@@ -1,1 +1,2 @@
 for /r %%g in (*.hdr) do gdal_translate -ot Float32 -of PCRaster -mo PCRASTER_VALUESCALE=VS_SCALAR "%%g" "%%~dpng.map"
+rem You can use the -projwin argument to clip the result to the area in question.  For Colorado, -projwin -110 41 -102 37
